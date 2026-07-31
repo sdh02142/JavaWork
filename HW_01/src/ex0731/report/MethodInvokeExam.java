bd877510f2fb5b93ca1219ba524c37842e76ad0e
PC <PC@DESKTOP-DNKIEGE>
2026-07-31 16:57:52 +0900

    edit : 2026-07-31 lecture finished.

---
+++ HW_01/src/ex0731/report/MethodInvokeExam.java
@@ -0,0 +1,44 @@
+package ex0731.report;
+
+class Methodinvoke01{
+	//다음과 같이 메소드를 작성하세요.
+	
+	/*method 이름 : method01
+	어디서나 누구나 접근가능
+	정수 리턴
+	인수로 정수2개 받음
+	인수로 들어온 정수 2개를 곱해서 출력하고 리턴*/
+		
+	public int method01(int a, int b) {
+		System.out.println(a*b);
+		return a*b;
+	}
+}
+
+class Methodinvoke02{
+	
+	//method 이름 : method02
+	//어디서나 누구나 접근가능,객체를 생성하지 않고도 접근가능
+	//정수 리턴
+	//인수로 정수2개 받음
+	
+	//인수로 들어온 정수 2개를 더해서 출력하고 더한수 리턴
+		
+	public static int method02(int a, int b) {
+		System.out.println(a+b);
+		return a+b;
+	}
+}
+
+public class MethodInvokeExam{
+	public static void main(String []args){
+		
+		//MethodInvoke01의 method01 호출
+		Methodinvoke01 mi01 = new Methodinvoke01();
+		mi01.method01(1, 2);
+
+		//MethodInvoke02의 method02 호출
+		Methodinvoke02.method02(1,2);
+	}
+}
+
---
+++ step01_basic/src/ex0731/method/B.java
@@ -0,0 +1,19 @@
+package ex0731.method;
+
+/*public X*/ class A {
+//	public static void main(String[] args) {
+//		System.out.println("A class operate");
+//	}
+}
+
+public class B {
+	public static void main(String[] args) {
+		System.out.println("B class operate");
+	}
+}
+
+class C {
+
+} 
+
+// java 파일을 저장할 때 컴파일이 일어나며, 해당 java 파일 내의 모든 클래스 별로 컴파일이 이루어져서 .class 파일들이 생성됨.
