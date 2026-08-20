bbddd3fe33e3b5b2be53f13ac3daed128a88f414
sdh02142 <sdh02142@naver.com>
2026-08-21 08:27:10 +0900

    2026-08-20 HW update

--- HW_01/src/ex0820/report/FileReadWrite.java
+++ HW_01/src/ex0820/report/FileReadWrite.java
@@ -1,45 +1,47 @@
 package ex0820.report;
 
+import java.io.File;
 import java.util.Scanner;
-
-import ex0805.constructor.goods.sol.EndView;
-import ex0805.constructor.goods.sol.Goods;
 
 public class FileReadWrite {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		String path = "src/ex0820/";
 		while(true){
-	         System.out.println("---------------------------------------------------------------------------");
-			 System.out.println("1.등록     2.전체검색      3.상품코드검색    4. 수정하기   5. 삭제하기   9.종료");
-			 System.out.println("----------------------------------------------------------------------------");
+	         System.out.println("--------------------------------------");
+			 System.out.println("1.프로필 저장     2.프로필 불러오기      3.종료");
+			 System.out.println("--------------------------------------");
 
 	         System.out.print("메뉴 선택 > ");
 			 int menuChoice = Integer.parseInt( sc.nextLine());
 
 			 switch(menuChoice){
 	             case 1 : 
-					 this.inputInsert() ; break;
+	            	 System.out.print("이름 > ");
+	            	 String name = sc.nextLine();
+	            	 path += (name+".txt");
+	            	 File file = new File(path);
+	             	 if(file.exists()) {
+	             		 System.out.println("중복된 이름");
+	             		 break;
+	             	 } else {
+	             		 System.out.print("상품가격 > ");
+	             		 int weight = Integer.parseInt(sc.nextLine());
+
+	             		 System.out.print("상품가격 > ");
+	             		 int password = Integer.parseInt(sc.nextLine());
+	             		 
+	             		 break;
+	             	 }
+					 
 				 case 2 : 
-					 Goods [] goodsArr = service.selectAll(); 
-				     EndView.printSelectAll(goodsArr);
-				      break;
-				 case 3 : 
-					 this.inputSelectByCode();
-					 break;
-				 case 4 : 
-					 this.inputUpdate() ; 
-					 break;
-				 case 5:
-					 this.inputDeleteCode();
-						break;	 
-				 case 9 : 
-					 System.out.println("다음에 또 이용해ㅜㅈ세요. 프로그램 종료합니다.");
+				     break;
+				 case 3 :  
+					 System.out.println("다음에 또 이용해주세요. 프로그램 종료합니다.");
 					 System.exit(0); //프로그램종료 메소드 
 				 break;
-				 default: System.out.println("메뉴는 1~4 or 9 만 입력해주세요.");
-
+				 default: System.out.println("메뉴는 1 ~ 3만 입력해주세요.");
 			 }//switch문
-
 		  }//while문
 	}
 
